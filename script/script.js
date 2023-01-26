774fbf7977e535ade9a35f99e8f0fedbf8253897
Martin <martinbungner@hotmail.com>
2023-01-26 16:27:44 +0100

    Revert "layout css"
    This reverts commit 9005a9194011ae994e4f057ea644f533e925d9a8.

--- script/script.js
+++ script/script.js
@@ -27,7 +27,7 @@ let timestamp;
 
 // input till databas
 const textInput = document.querySelector('#text-input');
-const usernameInput = document.querySelector('#username-input')
+const usernameInput = document.querySelector('#username')
 const textBtn = document.querySelector('#text-button');
 textBtn.addEventListener('click', createMessage);
 
